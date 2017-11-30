56b2929f66b209152ecf15adb2cca3a4cb8fc287
Nurtas Makhazhanov <nurtas@delta.alphacephei.com>
2017-11-30 18:13:10 +0100

    Add evaluation mode.

--- tests/g2p_unittest.py
+++ tests/g2p_unittest.py
@@ -2,6 +2,7 @@
 import unittest
 import tensorflow as tf
 import shutil
+import g2p_seq2seq.g2p as g2p
 from g2p_seq2seq.g2p import G2PModel
 from g2p_seq2seq.params import Params
 import inspect, os
@@ -17,8 +18,8 @@ class TestG2P(unittest.TestCase):
     params = Params(model_dir, train_path)
     g2p_model = G2PModel(params)
     g2p_model.prepare_data(train_path=train_path, dev_path=dev_path)
-    g2p_model.train()
-    shutil.rmtree(model_dir)
+    self.assertIsNone(g2p_model.train())
+    #shutil.rmtree(model_dir)
 
   def test_decode(self):
     model_dir = os.path.abspath("tests/models/decode")
@@ -33,6 +34,20 @@ class TestG2P(unittest.TestCase):
     self.assertEqual(out_lines[0].strip(), u"")
     self.assertEqual(out_lines[1].strip(), u"")
     self.assertEqual(out_lines[2].strip(), u"")
+
+  def test_evaluate(self):
+    model_dir = os.path.abspath("tests/models/decode")
+    test_path = os.path.abspath("tests/data/toydict.graphemes2")
+    gt_path = os.path.abspath("tests/data/toydict.test")
+    params = Params(model_dir, test_path)
+    g2p_model = G2PModel(params)
+    g2p_model.prepare_data(test_path=test_path)
+    gt_lines = open(gt_path).readlines()
+    g2p_gt_map = g2p.create_g2p_gt_map(gt_lines)
+    g2p_model.evaluate(gt_path, test_path)
+    errors = g2p_model.calc_errors(g2p_gt_map, test_path)
+    self.assertAlmostEqual(float(errors)/len(g2p_gt_map), 1.000, places=3)
+
 
   #def test_evaluate(self):
   #  model_dir = "tests/models/decode"
